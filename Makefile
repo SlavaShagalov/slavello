c77047c41ea0c52070c70ecbe8371ed438823e9b
SlavaShagalov <slavikshagalov@gmail.com>
2024-04-14 18:51:57 +0300

    Добавлено: запуск бэкенда в докере, docker-compose, Makefile для управления бэкендом #4

---
+++ Makefile
@@ -0,0 +1,23 @@
+# ===== RUN =====
+.PHONY: build
+build:
+	docker compose -f docker-compose.yml build api
+
+.PHONY: up
+up:
+	docker compose -f docker-compose.yml up -d --build api
+	make logs
+
+.PHONY: stop
+stop:
+	docker compose -f docker-compose.yml stop
+
+.PHONY: down
+down:
+	docker compose -f docker-compose.yml down -v
+
+# ===== LOGS =====
+service = api
+.PHONY: logs
+logs:
+	docker compose logs -f $(service)
---
+++ internal/pkg/config/keys.go
@@ -0,0 +1,24 @@
+package config
+
+// Server
+const (
+	ServerName = "NAME"
+	ServerPort = "PORT"
+)
+
+// Postgres
+const (
+	PostgresHost     = "PG_HOST"
+	PostgresPort     = "PG_PORT"
+	PostgresDB       = "PG_DB"
+	PostgresUser     = "PG_USER"
+	PostgresPassword = "PG_PASSWORD"
+	PostgresSSLMode  = "PG_SSL_MODE"
+)
+
+// Redis
+const (
+	RedisHost     = "REDIS_HOST"
+	RedisPort     = "REDIS_PORT"
+	RedisPassword = "REDIS_PASSWORD"
+)
